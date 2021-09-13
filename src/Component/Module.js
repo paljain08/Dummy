e11d87a6c649cd2e5b9eedfc66bed6904bbb45c3
prajaktasutarbmis <89061585+prajaktasutarbmis@users.noreply.github.com>
2021-09-14 00:08:38 +0530

    All modules Files has uploaded

---
+++ src/Component/Module.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Module() {
+    return (
+        <div className="container">
+                <h1>Module Page</h1>
+        </div>
+    );
+}
+
+export default Module;
---
+++ src/Component/Project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project() {
+  return (
+    <div className="container">
+      <h1>Project Page</h1>
+    </div>
+  );
+}
+
+export default Project;
